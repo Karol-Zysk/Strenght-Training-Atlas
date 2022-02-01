59e904efa339afb93dcc5d32f733d93fbb981470
Karol Zyśk <trener.karolzysk@gmail.com>
2022-02-02 00:53:08 +0100

    using react-querry to fetch data Api

--- src/components/Main/MainInfo/TrainingInfo.js
+++ src/components/Main/MainInfo/TrainingInfo.js
@@ -1,16 +1,31 @@
 import React from "react";
 import { useQuery } from "react-query";
+import style from "./TrainingInfo.module.css";
+import { loremIpsum } from "react-lorem-ipsum";
 
 const TrainingInfo = () => {
+  const [bodyParts, setBodyParts] = React.useState(
+    "https://exercisedb.p.rapidapi.com/exercises"
+  );
+
+  const back = "https://exercisedb.p.rapidapi.com/exercises/bodyPart/back";
+  const waist = "https://exercisedb.p.rapidapi.com/exercises/bodyPart/waist";
+
+  const handleBodyPartChange = (prev) => {
+    setBodyParts((prev = back));
+    console.log(prev);
+  };
+
   const fetchBodyParts = async () => {
     const response = await fetch(
-      "https://exercisedb.p.rapidapi.com/exercises",
+      `
+      ${bodyParts}`,
       {
         method: "GET",
         headers: {
           "x-rapidapi-host": "exercisedb.p.rapidapi.com",
           "x-rapidapi-key":
-            "xxx",
+            "aeffdb04d0mshe7c5d05286567c1p1d6819jsn1024fe6655c0",
         },
       }
     );
@@ -18,21 +33,27 @@ const TrainingInfo = () => {
   };
 
   const { data, status } = useQuery("bodyParts", fetchBodyParts);
-  console.log(data);
 
+  console.log(data);
+  const bodyy = data?.slice(0, 9)?.map((bodyPart) => (
+    <>
+      <div className={style.exerciseTile}>
+        <p className={style.paragraph}> {bodyPart.name.toUpperCase()}</p>
+        <img src={bodyPart.gifUrl} alt="img" className={style.exImg}></img>
+        <p className={style.paragraph}>
+          Target muscle: {bodyPart.target.toUpperCase()}
+        </p>
+      </div>
+    </>
+  ));
   return (
-    <div>
-      {data?.map((bodypart) => (
-        <div>
-          {bodypart.bodyPart === "upper arms" ? (
-            <div>
-              {bodypart.name.slice(0, 5)}
-              <img src={bodypart.gifUrl.slice(0, 5)} alt="img"></img>
-            </div>
-          ) : null}
-        </div>
-      ))}
-    </div>
+    <>
+      <button onClick={handleBodyPartChange}>C</button>
+      <nav style={{ width: "100%" }}>
+        <h2>Body Parts</h2>
+      </nav>
+      <div className={style.tileContainer}>{bodyy}</div>
+    </>
   );
 };
 
