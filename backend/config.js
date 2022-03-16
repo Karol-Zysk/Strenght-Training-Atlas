472a3450a19fc59a34360bdd03d5b928ffde3a23
Karol Zyśk <trener.karolzysk@gmail.com>
2022-03-16 23:55:58 +0100

    cleaning code

--- backend/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 3001,
-  database:
-    process.env.MONGODB_URI ||
-    "mongodb+srv://DevKarol:Mongo123@cluster1.nxmuh.mongodb.net/strenghtbible?retryWrites=true&w=majority",
-};
